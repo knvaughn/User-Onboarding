9f9a73aee04a9c3ccda2823e7e0c787cd23313b6
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-02-16 21:20:12 -0500

    Add form inputs

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -3,7 +3,37 @@ import React from 'react';
 function Form() {
     return (
         <div>
-            
+            <form>
+                <label>
+                    Name 
+                    <input 
+                        name="name"
+                        type="text"
+                    />
+                </label>
+                <label>
+                    Email 
+                    <input 
+                        name="email"
+                        type="email"
+                    />
+                </label>
+                <label>
+                    Password 
+                    <input 
+                        name="password"
+                        type="password"
+                    />
+                </label>
+                <label>
+                    Terms of Service
+                    <input 
+                        name="termsOfService"
+                        type="checkbox"
+                    />
+                </label>
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
