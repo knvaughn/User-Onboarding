dfcb91f1e42ace251e61eb4aa02ad58facdbd80b
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-02-16 21:59:16 -0500

    Add post request

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
+import axios from 'axios';
 
 const initialForm = {
   name: '',
@@ -12,16 +13,27 @@ const initialForm = {
 function App() {
   const [form, setForm] = useState(initialForm);
 
-  useEffect(() => {
-    
-  }, []);
+  const addNewUser = (newUser) => {
+    axios.post('https://reqres.in/api/users', newUser)
+    .then((response) => {
+      console.log(response);
+      setForm(initialForm);
+    })
+    .catch((error) => console.log(error));
+  }
 
   const inputChange = (name, value) => {
     setForm({...form, [name]: value});
   }
 
   const submit = () => {
-
+    const newUser = {
+      name: form.name.trim(),
+      email: form.email.trim(),
+      password: form.password.trim(),
+      termsOfService: !!form.termsOfService
+    }
+    addNewUser(newUser);
   }
 
   return (
