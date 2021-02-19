48308387126b80d016cc4ff0a4d06013581c0d0e
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-02-18 22:36:35 -0500

    Test whether user can fill out inputs

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -8,7 +8,20 @@ describe("Form Application", () => {
     const password = () => cy.get('[data-cy=password]');
     const termsOfService = () => cy.get('[data-cy=termsOfService]');
 
-    it("Can fill out name input", () => {
-        
+    it("Can fill out inputs", () => {
+        name()
+            .should('have.value', '')
+            .type('Kristin Vaughn')
+            .should('have.value', 'Kristin Vaughn');
+
+        email()
+            .should('have.value', '')
+            .type('vaughn.kristin.n@gmail.com')
+            .should('have.value', 'vaughn.kristin.n@gmail.com');
+
+        password()
+            .should('have.value', '')
+            .type('12345')
+            .should('have.value', '12345');
     }); 
 });
